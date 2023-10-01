ccadbdb631c07e5612865db6efe4a11b88bbdccf
Nishanth Reddy <nishroop1999@gmail.com>
2023-10-01 19:09:07 +0530

    worked with functions

---
+++ src/beginner1/functionToUse.java
@@ -0,0 +1,16 @@
+package beginner1;
+
+import java.util.Scanner;
+
+public class functionToUse {
+    public static void volumeOfSphere(final int A) {
+        double pi = Math.PI;
+        int product = (int)Math.ceil((4.0*pi*A*A*A)/(double)3.0);
+        System.out.print(product);
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        final int A = s.nextInt();
+        volumeOfSphere(A);
+    }
+}
