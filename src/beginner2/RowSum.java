5c51087849190d4e7791b08bb3017c146f9358e0
Nishanth Reddy <nishroop1999@gmail.com>
2023-10-02 19:22:49 +0530

    added a new solution to beginner2 and updated java docs

---
+++ src/beginner2/RowSum.java
@@ -0,0 +1,32 @@
+package beginner2;
+
+import java.util.Scanner;
+
+public class RowSum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(), m = s.nextInt();
+        int mat[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat[i][j] = s.nextInt();
+            }
+        }
+        int result[] = rowSum(mat);
+        for(int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    private static int[] rowSum(int[][] mat) {
+        int []result = new int[mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < mat[0].length; j++) {
+                sum += mat[i][j];
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+}
